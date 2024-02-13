a7b3673c7a75d588c38d8de463324d4f99d7a16a
CosimoA <cosimo.archip@gmail.com>
2024-02-13 17:53:03 +0100

    SETUPE: config Base scaffolding

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guest\PageController;
 
-Route::get('/', function () {
-    return view('pages.index');
-})->name('homepage');
+// Route::get('/', function () {
+//     return view('pages.home');
+// })->name('Homepage');
+
+Route::get('/', [PageController :: class, 'home']);
